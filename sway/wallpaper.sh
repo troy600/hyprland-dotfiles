74c1dfd304d289c4a042fbdd57608c2053c2f02c
troy600 <bazartroy531@gmail.com>
2024-09-11 12:38:31 +0800

    some updates

--- sway/wallpaper.sh
+++ sway/wallpaper.sh
@@ -1,3 +1,3 @@
-notify-send "setting up colors" & wal -i "$HOME/wallpaper/Arch-chan_to.png" --saturate 1 && notify-send "colors from wallpaper sucesfully" &
+notify-send "setting up colors" & wal -i "$HOME/wallpaper/Aiabstract.png" --saturate 1 && notify-send "colors from wallpaper sucesfully" &
 #swaybg -i "$HOME/wallpaper/aihoshino3.jpg" -m fill &
 exec ~/.azotebg
