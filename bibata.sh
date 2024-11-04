4b55d3207806046d1d9352d119f2bfcd7785c52a
troy600 <bazartroy531@gmail.com>
2024-11-04 16:02:59 +0800

    bibata cursor

---
+++ bibata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bibata_install() {
+    cp ./Bibata-Pink-Ice ~/.local/share/icons/Bibata-Pink-Ice
+    cp ./default/* ~/.local/share/icons/default/
+    cp ./default/* ~/.icons/default/
+}
