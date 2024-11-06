032ca61072b0355163d0b07cf1c440a6e29bbe41
troy600 <bazartroy531@gmail.com>
2024-11-06 16:50:41 +0800

    added login.sh(made by me)

---
+++ waybar/login.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+
+login=$(echo "Exit|Shutdown|Restart|Suspend|Terminal" | rofi -dmenu -no-X -sep "|"  -p "Want to shudown? or Restart?" -window-title "Power Menu")
+
+dmenu() {
+    echo "$1" | rofi -p "Are you sure about that?" -dmenu -no-X -window-title "login" -sep "|"
+}
+
+shutdown_prompt() {
+    dmenu 'Yes|No'
+}
+
+Shutdown() {
+    opts=$(shutdown_prompt)
+    case "$opts" in 
+        "Yes")
+            systemctl poweroff
+            ;;
+        "No")
+            rofi -e "cancelled"
+            return 1
+            ;;
+        esac
+}
+
+Restart() {
+    opts=$(dmenu 'Yes|No')
+    case "$opts" in
+        "Yes")
+            systemctl reboot
+            ;;
+        "No")
+            rofi -e "canceled"
+            return 1
+            ;;
+    esac
+
+}
+
+
+
+case "$login" in
+    "Exit")
+        exit
+        ;;
+    "Shutdown")
+        Shutdown
+        ;;
+    "Restart")
+        Restart
+        ;;
+    "Terminal")
+        rofi-sensible-terminal
+        ;;
+    "Suspend")
+        systemctl suspend
+        ;;
+esac
