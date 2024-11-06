5686a456b7e21a3ad20098436ada67c769ca73f1
troy600 <bazartroy531@gmail.com>
2024-11-06 16:50:28 +0800

    updates with login.sh(made by me)

--- hypr/wallpaper.sh
+++ hypr/wallpaper.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-#By Ali and ChatGPT-4o-mini
+#By Ali
 
 wallpaper_dir="$(ls ~/wallpaper)"
 
 # Show the menu using Rofi
-chosen=$(echo -e "$wallpaper_dir" | rofi -dmenu -i -p "Choose a wallpaper:3" -icons )
+chosen=$(echo -e "$wallpaper_dir" | rofi -dmenu -i -p "Choose a wallpaper:3")
 path="~/wallpaper/$chosen"
+wallpaper_cache="$HOME/.cache/currentwallpaper.png"
 
 if [[ -z "$chosen" ]]; then
 	notify-send "you didn't pick any wallpaper"
 	return 1
 else
-	exec waypaper --wallpaper $path &
+	notify-send "setting the wallpaper" &
+	waypaper --wallpaper $path  &
+	wal -i "$HOME/wallpaper/$chosen" --saturate 1.0 -n 
+	pkill waybar
+	waybar -c ~/.config/waybar/config-hyprland.jsonc &
+	cp -f "$HOME/wallpaper/$chosen" $wallpaper_cache
+	notify-send "success!!!!"
+	return 0
 fi
+
+
