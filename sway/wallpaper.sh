1c4ca22a04e3fff00d878461aa303568400e9cf9
troy600 <bazartroy531@gmail.com>
2024-11-26 18:29:09 +0800

    new wallpaper setter for sway(Hyprland soon)

--- sway/wallpaper.sh
+++ sway/wallpaper.sh
@@ -6,17 +6,39 @@ wallpaper_dir="$(ls ~/wallpaper)"
 wallpaper="$HOME/.cache/currentwallpaper.png"
 
 # Show the menu using Rofi
-chosen=$(echo -e "$wallpaper_dir" | rofi -dmenu -i -p "Choose a wallpaper:3" -l 17 )
-path="~/wallpaper/$chosen"
 
-notify-send "setting the wallpaper"
-if [[ -z "$chosen" ]]; then
-	notify-send "you didn't pick any wallpaper"
-	return 1
-else
-	waypaper --wallpaper $path  &
-	wal -i "$HOME/wallpaper/$chosen" --saturate 1.0 -n
-	pkill waybar
-	waybar -c ~/.config/waybar/config-sway.jsonc &
-	cp $HOME/wallpaper/$chosen $wallpaper
+SelectWallpaper() {
+    chosen=$(echo -e "$wallpaper_dir" | rofi -dmenu -i -p "Choose a wallpaper:3" -l 17 )
+}
+
+RandomWallpaper() {
+    chosen=$(echo "$wallpaper_dir" | shuf -n 1 )
+}
+
+
+main() {
+    notify-send "setting the wallpaper"
+    if [[ -z "$chosen" ]]; then
+	    notify-send "you didn't pick any wallpaper"
+	    return 1
+    else
+	    waypaper --wallpaper $path  &
+	    wal -i "$HOME/wallpaper/$chosen" --saturate 1.0 -n
+	    pkill waybar
+	    waybar -c ~/.config/waybar/config-sway.jsonc &
+	    cp $HOME/wallpaper/$chosen $wallpaper
 fi
+}
+
+if [[ -n $@ ]]; then
+	RandomWallpaper
+    path="~/wallpaper/$chosen"
+
+	main
+else
+	SelectWallpaper
+    path="~/wallpaper/$chosen"
+
+	main
+fi
+
