5b78ab36bd18ab1e33a434101af7f62c3c2e8633
troy600 <bazartroy531@gmail.com>
2024-12-02 20:01:27 +0800

    revsmping

--- install-revamped.sh
+++ install-revamped.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+noneed=--noconfirm --needed
+
 essentials() {
    system="systemd networkmanager pulseaudio pulsemixer python vim nano grub "
    misc="python-pywal16 waybar noto-fonts-extra noto-fonts-cjk ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono otf-fonts-awesome"
@@ -10,7 +12,7 @@ WmHyprland() {
 }
 
 yayInstall() {
-   yay
+   bash ./scripts/yay.sh
 }
 
 preffered_kernel() {
